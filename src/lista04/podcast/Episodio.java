c557fee64c142e93f9408b242234ee87dbc27d59
rogeriocsl <carlosrog1414@hotmail.com>
2026-03-16 16:42:40 -0300

    segundo commit

--- src/lista04/podcast/Episodio.java
+++ src/lista04/podcast/Episodio.java
@@ -21,7 +21,50 @@ public class Episodio {
             throw new IllegalArgumentException("Duração de minutos deve ser maior que zero.");
         }
 
-
+        this.titulo = titulo;
+        this.numero = numero;
+        this.duracaoMinutos = duracaoMinutos;
+        this.status = "RASCUNHO";
     }
 
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        if (duracaoMinutos <= 0) {
+            throw new IllegalArgumentException("A Duração deve ser maior que zero.");
+        }
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+
+    public int getDuracaoMinutos(){
+        return duracaoMinutos;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void publicar(){
+        if(getStatus().equals("RASCUNHO")){
+            this.status = "PUBLICADO";
+        }
+    }
+
+    public void arquivar(){
+        if(getStatus().equals("PUBLICADO")){
+            this.status = "ARQUIVADO";
+        }
+    }
+
+    public void restaurar(){
+        if(getStatus().equals("ARQUIVADO")){
+            this.status = "RASCUNHO";
+        }
+    }
 }
