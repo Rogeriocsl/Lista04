0266bbf40e363a5896bf0bc96affe646e52e5ce7
rogeriocsl <carlosrog1414@hotmail.com>
2026-03-16 23:02:43 -0300

    commit 2d

--- src/lista04/musica/Musica.java
+++ src/lista04/musica/Musica.java
@@ -6,7 +6,7 @@ public class Musica {
     int duracaoSegundos;
     String prioridade;
 
-    public Musica (String titulo, String artista, int duracaoSegundos) {
+    public Musica (String titulo, String artista) {
 
         if (titulo == null || titulo.trim().isEmpty()) {
             throw new IllegalArgumentException("Titulo não pode ser nulo ou vazio.");
@@ -16,12 +16,20 @@ public class Musica {
             throw new IllegalArgumentException("Artista não pode ser nulo ou vazio.");
         }
         if(duracaoSegundos <= 0){
-            throw new IllegalArgumentException("Valor invalido.");
+            throw new IllegalArgumentException("Valor Invalido.");
         }
 
         this.titulo = titulo;
         this.artista = artista;
         this.duracaoSegundos = duracaoSegundos;
 
+    }
+
+    public void setPrioridade(String valor){
+        if(!valor.equals("NORMAL")|| !valor.equals("URGENTE")){
+            throw new IllegalArgumentException("Valor Invalido.");
+        }else{
+            this.prioridade = valor;
+        }
     }
 }
--- src/lista04/musica/Playlist.java
+++ src/lista04/musica/Playlist.java
@@ -1,4 +1,20 @@
 package lista04.musica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
+    String nome;
+    List<String> ingredientes = new ArrayList<>();
+
+    public Playlist (String nome) {
+
+        if (nome.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nome não pode ser nulo ou vazio.");
+        }
+
+        this.nome = nome;
+
+
+    }
 }
